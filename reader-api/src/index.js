34bc0dc8db8fdf7f32982996e16b368f213823e9
meng yan <yanmeng0726@gmail.com>
2023-04-06 16:16:30 -0600

    changed to lowercase directory name

---
+++ reader-api/src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import router from './routes/index.js';
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//routes
+app.use(router);
+
+app.listen(process.env.PORT || 3000);
+
+console.log('Server on port 3000');
+export default app;
